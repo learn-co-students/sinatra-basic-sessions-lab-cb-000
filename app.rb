1d089ef4fc050256ce53ba982abc93b15d402a87
David Slivken <slivken@gmail.com>
2017-02-11 17:34:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "huckleberry"
+  end
+
+  get '/' do
+#    @session = sessions
+    erb :index
+  end
+
+  post '/checkout' do
+    session["item"] = params[:item]
+    @session = session
+    "The item is #{@session["item"]}"
+  end
+
 end
