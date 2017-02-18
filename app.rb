c91904db64a951ab431e1b27c572044dfbf5ce74
dustinlikens <dustinlikens@icloud.com>
2017-02-18 02:07:20 +0100

    Done.

--- app.rb
+++ app.rb
@@ -13,8 +13,9 @@ class App < Sinatra::Base
   end
 
   post "/checkout" do
-    item = "#{params[:item]}"
-    session[:item] = item
+    @item = "#{params[:item]}"
+    session[:item] = @item
+    erb :cart
   end 
 
 end
