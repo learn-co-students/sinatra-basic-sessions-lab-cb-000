900f013646d0cfa901c681b5be00ccc4fb9e9d07
Michael Anderson <michanders@msn.com>
2018-01-07 20:05:26 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "item"
+  end
+
+  get '/' do
+    resp = Rack::Response.new
+    resp.status = 200
+
+
+    erb :index
+  end
+
+  post '/checkout' do
+    @session = session
+    @session[:item] = params[:item]
+    erb :items
+  end
 end
