27d3e18594aba5fb5142ce97f40942be2d970678
Muhammad Zeeshan Shafqat <101041699@student.swin.edu.au>
2018-01-07 03:39:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "isomorphicelectrolytesthatcreatevolatilechemicals"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    #raise params.inspect
+    session[:item] = params[:item]
+    #raise session.inspect
+    @session = session
+    #raise @session.inspect
+    #binding.pry
+    "#{@session[:item]}"
+  end
 end
