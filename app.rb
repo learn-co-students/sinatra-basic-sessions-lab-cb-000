ce7f49afc5bb8dfb5133c0cd9ddeb798dcda1f5a
Heather Christman <efl7a@yahoo.de>
2018-01-14 14:36:02 -0500

    Complete but with questions

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    @session = session
+    erb :index
+  end
+
+  get '/item' do
+    @session = session
+    erb :item
+  end
+
+  post '/checkout' do
+    @session = session
+    @session[:item] = params[:item]
+
+    redirect '/item'
+  end
 end
