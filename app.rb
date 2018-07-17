0564434a3a6fa775942a43fc271603835cb8fbd6
Felice Forby <feliceforby@gmail.com>
2018-07-17 23:02:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "fuwafuwa"
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  get '/cart' do
+    erb :cart
+  end
+  
+  post '/checkout' do
+    session["item"] = params[:item]
+    @session = session
+
+    redirect '/cart'
+  end
 end
